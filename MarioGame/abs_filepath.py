e254611aaa2e31dfc9234c1c3e662a67fce9fe81
LuterGS <koo04034@gmail.com>
2021-05-14 10:50:31 +0900

    - pyGame 모듈 폴더화

---
+++ MarioGame/abs_filepath.py
@@ -0,0 +1,4 @@
+import os
+
+REAL_PATH = os.path.dirname(os.path.realpath(__file__)) + "/"
+ABS_PATH = os.path.dirname(os.path.abspath(__file__)) + "/"
---
+++ test.py
@@ -0,0 +1,5 @@
+from MarioGame.main import main
+
+exitmessage = 'restart'
+while exitmessage == 'restart':
+    exitmessage = main()
